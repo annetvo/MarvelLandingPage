a5b8cc7b72ad3cdad44fad02ffbf466a75f77935
annetvo <annie@MacBook-Pro.local>
2021-09-29 00:32:31 -0400

    updated commit

--- app/movies/routes.py
+++ app/movies/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 
 movies = Blueprint('movies', __name__, template_folder='movies_templates')
 
-@movies.route('/history')
-def history():
+@movies.route('/timeline')
+def timeline():
     comic_universe='Marvel Studios'
-    return render_template('history.html', universe=comic_universe)
+    return render_template('timeline.html', universe=comic_universe)
